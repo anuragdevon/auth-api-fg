024bb8097e0c60292617b867e9083aacf58c917f
anuragdevon <anuragdevon@users.noreply.github.com>
2023-03-22 04:21:01 +0000

    format code using black - bot

--- src/test/test.py
+++ src/test/test.py
@@ -1,33 +1,32 @@
 import requests
 import json
 
+
 def user_signup():
     url = "localhost:8080/auth/user/signup"
 
-    payload = json.dumps({
-    "email": "wkigykikfgskxtngvz@tmmbt.com",
-    "password": "12345678",
-    "name": "anuragdevon",
-    "avatar": ""
-    })
-    headers = {
-    'Content-Type': 'application/json'
-    }
+    payload = json.dumps(
+        {
+            "email": "wkigykikfgskxtngvz@tmmbt.com",
+            "password": "12345678",
+            "name": "anuragdevon",
+            "avatar": "",
+        }
+    )
+    headers = {"Content-Type": "application/json"}
 
     response = requests.request("POST", url, headers=headers, data=payload)
 
     print(response.text)
 
+
 def user_signin():
     url = "localhost:8080/auth/user/login"
 
-    payload = json.dumps({
-    "email": "wkigykikfgskxtngvz@tmmbt.com",
-    "password": "12345678"
-    })
-    headers = {
-    'Content-Type': 'application/json'
-    }
+    payload = json.dumps(
+        {"email": "wkigykikfgskxtngvz@tmmbt.com", "password": "12345678"}
+    )
+    headers = {"Content-Type": "application/json"}
 
     response = requests.request("POST", url, headers=headers, data=payload)
 
@@ -37,60 +36,56 @@ def user_signin():
 def user_update():
     url = "localhost:8080/auth/user/update"
 
-    payload = "{\n    \"id_token\": "",\n    \"refresh_token\": "",\n    \"user_data\": {\n        \"name\": \"kakashi\"\n    }\n}"
-    headers = {
-    'Content-Type': 'application/json'
-    }
+    payload = (
+        '{\n    "id_token": '
+        ',\n    "refresh_token": '
+        ',\n    "user_data": {\n        "name": "kakashi"\n    }\n}'
+    )
+    headers = {"Content-Type": "application/json"}
 
     response = requests.request("POST", url, headers=headers, data=payload)
 
     print(response.text)
+
 
 def user_logout():
     url = "localhost:8080/auth/user/logout"
 
-    payload = "{\n    \"id_token\": "",\n    \"refresh_token\": ""\n}"
-    headers = {
-    'Content-Type': 'application/json'
-    }
+    payload = '{\n    "id_token": ' ',\n    "refresh_token": ' "\n}"
+    headers = {"Content-Type": "application/json"}
 
     response = requests.request("POST", url, headers=headers, data=payload)
 
     print(response.text)
+
 
 def delete():
     url = "localhost:8080/auth/user/delete"
 
-    payload = "{\n    \"id_token\": "",\n    \"refresh_token\": ""\n}"
-    headers = {
-    'Content-Type': 'application/json'
-    }
+    payload = '{\n    "id_token": ' ',\n    "refresh_token": ' "\n}"
+    headers = {"Content-Type": "application/json"}
 
     response = requests.request("POST", url, headers=headers, data=payload)
 
     print(response.text)
+
 
 def user_password_reset():
     url = "localhost:8080/auth/user/password/reset"
 
-    payload = json.dumps({
-    "email": "wkigykikfgskxtngvz@tmmbt.com"
-    })
-    headers = {
-    'Content-Type': 'application/json'
-    }
+    payload = json.dumps({"email": "wkigykikfgskxtngvz@tmmbt.com"})
+    headers = {"Content-Type": "application/json"}
 
     response = requests.request("POST", url, headers=headers, data=payload)
 
     print(response.text)
 
+
 def user_get():
     url = "localhost:8080/auth/user/get"
 
-    payload = "{\n    \"id_token\": "",\n    \"refresh_token\": ""\n}"
-    headers = {
-    'Content-Type': 'application/json'
-    }
+    payload = '{\n    "id_token": ' ',\n    "refresh_token": ' "\n}"
+    headers = {"Content-Type": "application/json"}
 
     response = requests.request("GET", url, headers=headers, data=payload)
 
